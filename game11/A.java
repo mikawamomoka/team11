6ee2176b52ba49176943d646f602ce991649f3e0
mikawamomoka <18162@hakodate.kosen-ac.jp>
2022-12-01 09:38:59 +0900

    gitignore 更新

--- game11/A.java
+++ game11/A.java
@@ -15,5 +15,9 @@ public class A extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "right" ) ){
+        setRotation(0);
+        move(1);
+}
     }    
 }
