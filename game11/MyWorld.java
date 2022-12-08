f948c90ea3c6c69c8a0546c98c4ea19a20bd3bcf
eviflyy <18169@hakodate.kosen-ac.jp>
2022-12-08 09:54:32 +0900

    (自分の記号(A〜D))がキャラ移動処理を追加

--- game11/MyWorld.java
+++ game11/MyWorld.java
@@ -18,5 +18,7 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         addObject( new taro(),100,100);
+        addObject( new jirai(),200,100);
+        addObject( new num1(),300,100);
     }
 }
---
+++ game11/num1.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class num1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class num1 extends Actor
+{
+    /**
+     * Act - do whatever the num1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
