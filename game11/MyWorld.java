1619f224aa239838f53fdbbd27f3c7b7365753e6
eviflyy <18169@hakodate.kosen-ac.jp>
2023-02-16 09:16:04 +0900

    新規シナリオ作成

--- game11/MyWorld.java
+++ game11/MyWorld.java
@@ -6,8 +6,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class MyWorld extends World
 {
+     GreenfootImage back;
+    GreenfootImage back_flop;
+    int back_dx = -5; // スクロール速度(マイナスにすると左から右)
+    int back_x = 0;
+    int back_width;
+    boolean flop = false;
 
     /**
      * Constructor for objects of class MyWorld.
@@ -15,12 +22,17 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
+        
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(850, 450, 1); 
         addObject( new taro(),100,100);
        // addObject( new jirai(),200,100);
-        addObject( new num1(),300,100);
+       //addObject( new num1(),300,100);
+       back = new GreenfootImage( "./images/chisou_mizu.png" );
+        back_flop = new GreenfootImage( "./images/chisou_hanten.png" );
+        back_width = back.getWidth();
         
+       
            for(int i=0; i<3; ++i)
         {
             
@@ -30,12 +42,39 @@ public class MyWorld extends World
               A = 0;
               B = 300;
              int y = A + (int)(Math.random()*((B-A)+1));
-             addObject( new jirai(), x, y );   
-            
+             addObject( new hata(), x, y );   
+             
 
             }
 
-            setActOrder(jirai.class, num1.class, taro.class);
+            setActOrder(jirai.class,hata.class, taro.class);
+    }
+    public void act()
+    {
+        back_x += back_dx;
+        if( back_x > 0){
+            back_x -= back_width;
+            flop = !flop;
+        }
+        if( back_x < -back_width ){
+            back_x += back_width;
+            flop = !flop;
+        }
+        getBackground().drawImage( flop ? back_flop : back, back_x, 0 );
+        getBackground().drawImage( flop ? back : back_flop, back_x+back_width, 0 );
+
+        showText( "back_x : "+ back_x, 100, 10 );
     }
     
+
 }
+
+
+
+ 
+
+
+
+     
+
+    
